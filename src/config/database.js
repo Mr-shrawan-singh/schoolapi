138e58008f7f5fcfc1a1c2dad734eadc83901dbf
Mr-shrawan-singh <shrawansingh9571@gmail.com>
2026-04-27 06:30:00 +0530

    update enve

--- src/config/database.js
+++ src/config/database.js
@@ -1,17 +1,6 @@
 import { Sequelize } from "sequelize";
 import { env } from "./env.js";
 
-// export const sequelize = new Sequelize(
-//   env.DB_NAME,
-//   env.DB_USER,
-//   env.DB_PASSWORD,
-//   {
-//     host: env.DB_HOST,
-//     dialect: "postgres",
-//     logging: false,
-//   }
-// );
-
 export const sequelize = new Sequelize(
   process.env.DB_URL ,
   {
