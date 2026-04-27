37c1703f3a7e03cc1fa9c65052ffcd49bccadb6e
Mr-shrawan-singh <shrawansingh9571@gmail.com>
2026-04-27 05:49:28 +0530

    add db url

--- src/config/database.js
+++ src/config/database.js
@@ -1,13 +1,27 @@
 import { Sequelize } from "sequelize";
 import { env } from "./env.js";
 
+// export const sequelize = new Sequelize(
+//   env.DB_NAME,
+//   env.DB_USER,
+//   env.DB_PASSWORD,
+//   {
+//     host: env.DB_HOST,
+//     dialect: "postgres",
+//     logging: false,
+//   }
+// );
+
 export const sequelize = new Sequelize(
-  env.DB_NAME,
-  env.DB_USER,
-  env.DB_PASSWORD,
+  process.env.DB_URL ,
   {
-    host: env.DB_HOST,
     dialect: "postgres",
     logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   }
 );
